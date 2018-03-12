d0e83c03657fcb6ae438e47d47ba9d76848f3d83
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-12 19:27:25 +0700

    update

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
                 <h2 class="post-title">
                   <?php the_title(); ?>
                 </h2>
-
               </a>
               <p class="post-meta">Posted by
                 <a href="#"><?php the_author(); ?></a>
