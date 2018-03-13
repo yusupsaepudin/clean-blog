0c8240dd6519d81a92882dde71253b5559b0d495
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-13 21:08:06 +0700

    dynamic site identity home

--- header.php
+++ header.php
@@ -50,8 +50,8 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="site-heading">
-              <h1>Clean Blog</h1>
-              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+              <h1><?php echo get_bloginfo( 'name' ); ?></h1>
+              <span class="subheading"><?php echo get_bloginfo( 'description' ); ?></span>
             </div>
           </div>
         </div>
