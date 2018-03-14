16b7cff9ef9039a8cf4b72407fe140cd9e9f5868
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-14 23:45:35 +0700

    update

--- functions.php
+++ functions.php
@@ -11,7 +11,6 @@ function cleanblog_enqueue_style() {
 add_action('wp_enqueue_scripts', 'cleanblog_enqueue_style');
 
 
-
 // ADD CLASS TO PAGINATION LINK
 add_filter('next_posts_link_attributes', 'posts_link_attributes_1');
 add_filter('previous_posts_link_attributes', 'posts_link_attributes_2');
